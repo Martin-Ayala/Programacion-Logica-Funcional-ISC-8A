d23b7e8d093a64c46c7893029bd421c9fc825ef9
Martin <martinadrianayala@gmail.com>
2026-03-19 14:56:24 -0500

    Texto de los ejercicios

--- Programacion_Funcional/Ejercicio_2.1/01_basico/ejercicios.py
+++ Programacion_Funcional/Ejercicio_2.1/01_basico/ejercicios.py
@@ -1,3 +1,8 @@
+###
+# exercises.py
+# Ejercicios para practicar los conceptos aprendidos en las lecciones. 
+###
+
 from os import system
 if system("clear") != 0: system("cls")
 
@@ -8,6 +13,38 @@ print("Escribe un programa que imprima tu nombre y tu ciudad en lineas separadas
 
 print("-------------------")
 
-print("\nEjercicio 2: Muestra los tipos de datos de las")
-print("")
-print("")
+print("\nEjercicio 2: Muestra los tipos de datos de las siguientes variables:")
+print("Usa el comando 'type()' para determinar el tipo de datos de cada variable.")
+a = 15
+b = 3.14159
+c = "Hola mundo"
+d = True
+e = None
+
+### Completa aquí
+
+print("------------------")
+
+print("\nEjercicio 3: Casting de tipos")
+print("Convierte la cadena \"12345\" a un entero y luego a un float.")
+print("Convierte el float 3.99 a un entero. ¿Qué ocurre?")
+
+### Completa aquí
+
+print("------------------")
+
+print("\nEjercicio 4: Variables")
+print("Crea variables para tu nombre, edad y altura.")
+print("Usa f-strings para imprimir una presentación.")
+
+# !Hola! Me llamo tu_nombre y tengo tu_edad años, mido tu_altura metros
+
+### Completa aquí
+
+print("------------------")
+
+print("\nEjercicio 5: Números")
+print("1. Crea una variable con el número PI (sin asignar una variable)")
+print("2. Redondea el número con round()")
+print("3. Haz la división entera entre el número que te salió y el número 2")
+print("4. El resultado debería ser 1")
