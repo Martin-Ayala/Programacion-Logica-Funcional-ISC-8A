688a66a9c5bd833b93d4ff59046e824606f07495
Martin <martinadrianayala@gmail.com>
2026-04-16 06:17:43 -0500

    Ciclos

---
+++ Programacion_Funcional/Ejercicio_2.1/03_ciclos/01_loop_while.py
@@ -0,0 +1,70 @@
+####
+# 01 - Bucles (while)
+# Permiten ejecutar un bloque de código mientras se cumpla una condición.
+####
+
+from os import system
+if system("clear") != 0: system("cls")
+
+print("\n Bucle while:")
+
+# Bucle con una simple condición
+contador = 0
+
+while contador <= 5:
+    print(contador)
+    contador += 1 # es super importante para evitar un bucle infinito
+
+#utilizando la palabra break, para romper el bucle
+print("\n Bucle while con breack:")
+contador = 0
+
+while True:
+    print(contador)
+    contador += 1
+    if contador == 5:
+        break # sale del bucle
+
+# continue, que lo hace es saltar esa iteracion en concreto
+# y continuar con el bucle
+print("\n Bucle while con continue:")
+contador = 0
+while contador < 10:
+    contador += 1
+
+    if contador % 2 == 0:
+        continue
+
+    print(contador)
+
+#else, esta condicion cuando se ejecuta?
+print("\n Bucle while con else:")
+contador = 0
+while contador < 5:
+    print(contador)
+    contador += 1
+else:
+    print("El bucle ha terminado")
+
+
+# pedirle al usuario un numero que tiene
+# que ser positivo si no, no le dejamos en paz
+numero = -1
+while numero < 0:
+    numero = int(input("Escribe un numero positivo: "))
+    if numero < 0:
+        print("El numero debe ser positivo. Intenta otra vez")
+    
+print(f"El numero que has introducido es {numero}")
+
+numero = -1
+while numero < 0:
+    try:
+        numero = int(input("Escribe un numero positivo: "))
+        if numero < 0:
+            print("El numero debe ser positivo. Intenta otra vez")
+    except:
+        print("Lo que introduces debe ser un numero!")
+
+###
+# EJERCICIOS (while)
---
+++ Programacion_Funcional/Ejercicio_2.1/03_ciclos/02_loop_for.py
@@ -0,0 +1,58 @@
+from os import system
+if system("clear") != 0: system("cls")
+
+# Iterar una lista
+frutas = ["manzana", "pera", "mandarina"]
+for fruta in frutas:
+    print(fruta)
+
+# Iterar sobre cualquier cosa que sea iterable
+cadena = "estudiante"
+for caracter in cadena:
+    print(caracter)
+
+# enumerate()
+frutas = ["manzana", "pera", "mandarina"]
+for idx, value in enumerate(frutas):
+    print(f"El indice es {idx} y la fruta es {value}")
+
+# bucles anidados
+letras = ["A", "B", "C"]
+numeros = [1, 2, 3]
+
+for letra in letras:
+    for numero in numeros:
+        print(f"{letra}{numero}")
+
+
+# break
+print("\nbreak:")
+animales = ["perro", "gato", "loro", "pez", "canario"]
+for idx, animal in enumerate(animales):
+    print(animal)
+    if animal == "loro":
+        print(f"El loro esta escondido en el indice {idx}")
+        break
+
+#enumerate lo que hace es devolver el indice y el valor de cada elemento de la lista
+
+# continue
+print("\ncontinue:")
+animales = ["perro", "gato", "loro", "pez", "canario"]
+for idx, animal in enumerate(animales):
+    if animal == "loro": continue
+
+    print(animal)
+
+# Comprension de listas (list comprehension)
+animales = ["perro", "gato", "loro", "pez", "canario"]
+animales_mayus = [animal.upper() for animal in animales]
+print(animales_mayus)
+
+# Muestra los numeros pares de una lista
+pares = [num for num in [1, 2, 3, 4, 5, 6] if num % 2 == 0]
+print(pares)
+
+###
+# EJERCICIOS (for)
+###
